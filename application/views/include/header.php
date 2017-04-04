12deffe59c5bafa0a4fcd6a09ec8b98fe3b5d742
Theophile Marcellus <theophilemarcellus@theos-mbp.attlocal.net>
2017-04-03 22:29:14 -0400

    Added Nova Multipurpose template to CodeIgniter 3

---
+++ application/views/include/header.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Home | Nova</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+
+    <link rel="stylesheet" href="<?php echo base_url(); ?>template/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>template/css/bootstrap-responsive.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>template/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>template/css/main.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>template/css/sl-slide.css">
+
+    <script src="<?php echo base_url(); ?>template/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="<?php echo base_url(); ?>template/images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url(); ?>template/images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url(); ?>template/images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url(); ?>template/images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo base_url(); ?>template/images/ico/apple-touch-icon-57-precomposed.png">
+</head>
+
+<body>
+
+    <!--Header-->
+    <header class="navbar navbar-fixed-top">
+        <div class="navbar-inner">
+            <div class="container">
+                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                <a id="logo" class="pull-left" href="<?php echo base_url(); ?>template/index.html"></a>
+                <div class="nav-collapse collapse pull-right">
+                    <ul class="nav">
+                        <li class="active"><a href="<?php echo base_url(); ?>template/index.html">Home</a></li>
+                        <li><a href="<?php echo base_url(); ?>template/about-us.html">About Us</a></li>
+                        <li><a href="<?php echo base_url(); ?>template/services.html">Services</a></li>
+                        <li><a href="<?php echo base_url(); ?>template/portfolio.html">Portfolio</a></li>
+                        <li class="dropdown">
+                            <a href="<?php echo base_url(); ?>template/#" class="dropdown-toggle" data-toggle="dropdown">Pages <i class="icon-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="<?php echo base_url(); ?>template/career.html">Career</a></li>
+                                <li><a href="<?php echo base_url(); ?>template/blog-item.html">Blog Single</a></li>
+                                <li><a href="<?php echo base_url(); ?>template/faq.html">FAQ</a></li>
+                                <li><a href="<?php echo base_url(); ?>template/pricing.html">Pricing</a></li>
+                                <li><a href="<?php echo base_url(); ?>template/404.html">404</a></li>
+                                <li><a href="<?php echo base_url(); ?>template/typography.html">Typography</a></li>
+                                <li><a href="<?php echo base_url(); ?>template/registration.html">Registration</a></li>
+                                <li class="divider"></li>
+                                <li><a href="<?php echo base_url(); ?>template/privacy.html">Privacy Policy</a></li>
+                                <li><a href="<?php echo base_url(); ?>template/terms.html">Terms of Use</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="<?php echo base_url(); ?>template/blog.html">Blog</a></li> 
+                        <li><a href="<?php echo base_url(); ?>template/contact-us.html">Contact</a></li>
+                        <li class="login">
+                            <a data-toggle="modal" href="<?php echo base_url(); ?>template/#loginForm"><i class="icon-lock"></i></a>
+                        </li>
+                    </ul>        
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
+    </header>
+    <!-- /header -->
+
+   
